aa8420267b058430253a08a032d9ede7744434d0
EvesK <mkessler@olin.edu>
2024-04-22 21:40:51 +0000

    removing commented out code

--- src/view.h
+++ src/view.h
@@ -54,21 +54,3 @@ int initialize_window(GameState* gameState);
  * renderer to destroy.
  */
 void destroy_window(GameState* gameState);
-
-// /**
-//  * Create game screen and render objects onto the screen
-//  *
-//  * When called, the background screen is rendered. The bird rect
-//  * is created, and that is also rendered onto the screen.
-//  *
-//  * @param gameState a pointer to an instance of GameState
-//  * @param bird a copy of the instance of Bird
-//  */
-
-// void render(GameState* gameState, Bird bird, Ground ground, Pipes pipes);
-
-// void render_start(GameState* gameState);
-
-// int initialize_window(GameState* gameState);
-
-// void destroy_window(GameState* gameState);
